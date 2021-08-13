8518d57e799968b43fd494cb09910195ecefcaf5
unknown <RockstarJay>
2021-08-13 15:22:30 +0530

    Added: creating a contact

--- src/com/jaybhagat/addressbook/AddressBookMain.java
+++ src/com/jaybhagat/addressbook/AddressBookMain.java
@@ -5,8 +5,53 @@ package com.jaybhagat.addressbook;
  * @author com.jaybhagat
  */
 
+import java.util.Scanner;
+
 public class AddressBookMain {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Address Book Program");
-	}
+	String firstName, lastName, address, city, state, email;
+    long phoneNumber, zipCode;
+
+    public AddressBookMain(String firstName, String lastName, String address, String city, String state, String email, long phoneNumber, long zipCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.zipCode = zipCode;
+    }
+    
+    // taking inputs from the user
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter First Name: ");
+        String firstName = scanner.next();
+        System.out.print("Enter Last Name: ");
+        String lastName = scanner.next();
+        System.out.print("Enter Address: ");
+        String address = scanner.next();
+        System.out.print("Enter City Name: ");
+        String city = scanner.next();
+        System.out.print("Enter State Name: ");
+        String state = scanner.next();
+        System.out.print("Enter Zip Code: ");
+        long zipCode = scanner.nextLong();
+        System.out.print("Enter Phone Number: ");
+        long phoneNumber = scanner.nextLong();
+        System.out.print("Enter Email Address: ");
+        String email = scanner.next();
+        scanner.close();
+
+        AddressBookMain addressbook = new AddressBookMain(firstName, lastName, address, city, state, email, phoneNumber, zipCode);
+        
+        System.out.println("First Name: " +addressbook.firstName);
+        System.out.println("Last Name: " +addressbook.lastName);
+        System.out.println("Address: " +addressbook.address);
+        System.out.println("City: " +addressbook.city);
+        System.out.println("State: " +addressbook.state);
+        System.out.println("Phone Number: " +addressbook.phoneNumber);
+        System.out.println("Email Address: " +addressbook.email);
+        System.out.println("Zip Code: " +addressbook.zipCode);
+    }
 }
