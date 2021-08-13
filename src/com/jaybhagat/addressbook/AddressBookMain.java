97651280b702ec8c9cdef023064bceb7e66f1946
unknown <RockstarJay>
2021-08-13 15:17:52 +0530

    Added: welcome message

---
+++ src/com/jaybhagat/addressbook/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.jaybhagat.addressbook;
+
+/*
+ * class for the message "Welcome to the Address Book Program" 
+ * @author com.jaybhagat
+ */
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Address Book Program");
+	}
+}
